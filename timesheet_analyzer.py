664850c53d61c64cdc0dba2cc3ecb316ae8d5bbf
arockiajerald1989 <55880483+arockiajerald1989@users.noreply.github.com>
2024-01-02 09:56:26 -0500

    Update timesheet_analyzer.py

--- timesheet_analyzer.py
+++ timesheet_analyzer.py
@@ -2,18 +2,23 @@ import pandas as pd
 
 # Load the Excel file
 df = pd.read_excel('output_file.xlsx')
+column_length = len(df.columns)
+print("column_length :", column_length)
 
 # Extract names and numbers using regular expressions
 df[['Name', 'Number']] = df['name'].str.extract(r"(.*) \((\d+)\)")
 
 df = df.drop('name', axis=1)
 
+column_length = len(df.columns)
+print("column_length :", column_length)
+
 # Extract regular and overtime hours
 overtime_threshold = 40
 df['Regular Hours'] = df['Total Hours'].clip(upper=overtime_threshold)
 df['Overtime Hours'] = df['Total Hours'] - df['Regular Hours']
 
-df = df[['Name', 'Number'] + list(df.columns[1:6]) + ['Total Hours'] + ['Regular Hours'] + ['Overtime Hours']]  # Rearrange columns as desired
+df = df[['Name', 'Number'] + list(df.columns[0:column_length-3]) + ['Total Hours', 'Regular Hours', 'Overtime Hours']]
 
 # Write the results to a new Excel file with auto-fitted columns
 with pd.ExcelWriter('extracted_data.xlsx', engine='openpyxl') as writer:
